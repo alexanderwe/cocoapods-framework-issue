1326dfc287f1f7318713e03b368e287e1317a25c
Alexander Weiß <alexander.weiss02@sap.com>
2021-12-28 15:16:28 +0100

    Initial Commit

---
+++ App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  App
+//
+//  Created by Weiss, Alexander on 28.12.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
